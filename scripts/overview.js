15da618990be7dffd1dbca5eec64ef3d2bde2239
macand842 <55792391+macand842@users.noreply.github.com>
2023-11-08 08:50:07 +0100

    third commit

--- scripts/overview.js
+++ scripts/overview.js
@@ -1,12 +1,23 @@
 fetch('JSON/sport.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    const title = document.title;
+
+    // Determine the array to use based on the document title
+    let articlesArray = [];
+    if (title === 'sport') {
+      articlesArray = data.sportartikler;
+    } else if (title === 'nyheder') {
+      articlesArray = data.nyhederartikler;
+    } else if (title === 'underholdning') {
+      articlesArray = data.underholdningartikler;
+    }
+
     const cards = document.querySelectorAll('.card');
 
-    data.sportartikler.forEach((articleData, index) => {
-      const card = cards[index]; // Holen Sie sich die Karte entsprechend des Index.
-      if (card) { // Überprüfen Sie, ob die Karte existiert.
+    articlesArray.forEach((articleData, index) => {
+      const card = cards[index];
+      if (card) {
         card.querySelector('.overskrift').textContent = articleData.overskrift;
         const image = card.querySelector('.billede');
         image.src = 'billeder/' + articleData.billede;
@@ -17,5 +28,3 @@ fetch('JSON/sport.json')
   .catch(error => {
     console.error('Fejl ved hentning af data: ', error);
   });
-
-
